66d13d087f708e2f0d17d050e07884454928e8b7
HarshYadav1159 <92823990+HarshYadav1159@users.noreply.github.com>
2023-03-29 20:19:10 +0530

    Add files via upload

---
+++ Revision.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.io.*;
+public class Revision {
+    public static void main(String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(4);
+        list.add(6);
+        list.add(5);
+        list.add(7);
+      
+        System.out.print( list.get(4));
+    }
+}
